8a5dd9b1b6e937ac28c11537ebedb16dee0df9c0
abdulla1923 <139139839+abdulla1923@users.noreply.github.com>
2023-07-10 19:32:27 +0500

    Create robot.cpp

---
+++ robot.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+   cout << " +\"\"\"\"\"+"<<endl;
+   cout << "[| O O |]"<<endl;
+   cout << " |  ^  |"<<endl;
+   cout << " | '_' |"<<endl;
+   cout << " +-----+"<<endl;
+   
+}
